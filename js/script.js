633f505a111e68c2a8b0c48c7e31d4b5ab79c2be
Krystal Lombera <klombera@gmail.com>
2016-06-28 11:19:58 -0700

    click on squares and switching turns

--- js/script.js
+++ js/script.js
@@ -1,9 +1,35 @@
-var board = ["X", "O", "", "", "", "X", "", "", ""]
+//models
+var board = ["X", "", "", "", "", "", "", "", ""]
 var turn = "X"
 
+//selectors
+var squares = document.querySelectorAll('.square')
+
+//set up click listeners for my squares
+for(var i = 0; i < 9; i++){
+  squares[i].addEventListener('click', function(e) {
+//loop through squares again and check which node was clicked
+    for(var j=0; j<9; j++) {
+      if (squares[j] === e.target) {
+        board[j] = turn
+        drawboard()
+        switchTurn()
+      }
+    }
+  })
+}
+
+//drawing model to the screen
 function drawboard () {
-  var squares = document.querySelectorAll('.square')
   for(var i = 0; i < 9; i++){
-    squares[i].innerHTML = board[i];
+    squares[i].innerHTML = board[i]
   }
 }
+
+//switching turns
+function switchTurn() {
+  if (turn === "X")
+    turn = "O"
+  else
+    turn = "X"
+  }
