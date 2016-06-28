180327aeae8066a8c8d387ec8315d6f977d8f402
Krystal Lombera <klombera@gmail.com>
2016-06-28 12:36:23 -0700

    reset button

--- js/script.js
+++ js/script.js
@@ -27,7 +27,14 @@ for(var i = 0; i < 9; i++){
 //     }
 //   })
 // }
-
+//set up click listener for Reset Button
+document.getElementById('reset').addEventListener('click', function (e) {
+  //reset board to blank values
+  for (var i=0; i<9; i++) {
+    board[i] = ""
+  }
+  drawboard()
+})
 //given an HTML element, it will determine the index of the square on the board
 function getSquareIndex(target) {
     for(var j=0; j<9; j++) {
